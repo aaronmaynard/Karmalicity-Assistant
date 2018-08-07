70202eed60cc49bc480dbf419b31aafaaed1e765
Aaron Maynard <34851708+aaronmaynard@users.noreply.github.com>
2018-08-07 04:51:52 -0500

    Polishing
    Removed redundant or unused variables and methods.  Did a bit of condensing.  Prepared to add two methods for delay and disable.

--- assistant/contentScript.js
+++ assistant/contentScript.js
@@ -1,139 +1,62 @@
 /*
-Global variables, loaded on every page open
+Global Variables
+Loaded on every page open
 */
-
-var title = window.location.href; // Gets the url of the webpage
-var credits;
-var runningTime;
+var title = window.location.href; // Gets the url of the current webpage
+var key; // Holds the current value of credits or running time
 var node; // All of the html for the node in reference to target id
-var targetId;
-var data;
+var targetId; // String used to find the DIV associated with the target
+var dLR // Daily Limit Reached
 
 /*
-Check which page we are loaded on
-Do we need a timeout function?
+Functions referenced by Page Validator
 */
-
-if (title == "http://www.karmalicity.com/get-points/") { // We have loaded on the startup page
-	
-	var urlListing = getListing(); // Gets the url of the next IMDb listing
-	openNewURLInTheSameWindow(urlListing); // The listing URL is now open on the current page
-	
-} else if (title.includes("listing")) { // We have loaded on the listing page
-	
-	// First check that we have not reached the daily limit
-	var activityCol = document.getElementById("rightColumn");
-	var tmpNode = document.createElement( "div" );
-	tmpNode.appendChild( activityCol.cloneNode( true ) );
-	var str = tmpNode.innerHTML;
-	// look for 60 out of 60
-	if (str.includes("You've completed 60 out of 60 possible actions so far today.")) { // completed daily activity
-		/*
-		Stop extension
-		*/
-		
-		
-	} else {
-		
-		targetId = getTargetId(); // Gets the target id
-		console.log(targetId);
-		var urlIMDb = document.querySelector(".btn-karma").getAttribute("data-url"); // Gets the listings IMDb link
-		
-		sendDataToExtension(targetId);
-		
-		document.querySelector(".btn-karma").click(); // Click the button
-		
-		/*
-		Extension should now be on IMDb page
-		*/
-		
-		setTimeout(function(){
-			getCreditsFromExtension("credits");
-			setTimeout(function(){
-				credits = credits.replace('key_','');
-				document.getElementById("imdb-view-answer").setAttribute('value',credits); // Fills credits
-				document.querySelector(".btn-success").click(); // Click the button
-			}, 1000);
-		}, 4000);
-	}
-	
-} else if (title.includes("imdb")) { // We have loaded on the IMDb page
-	// Get the targetId from the extension
-	setTimeout(function(){
-		getIdFromExtension("target");
-		setTimeout(function(){
-			console.log(targetId);
-			if (targetId == "Tips:"){
-				obtainRunningTime();
-				runningTime = "key_" + runningTime;
-				credits = runningTime;
-				sendDataToExtension(credits);
-			} else{
-				obtainCredits(targetId);
-				credits = "key_" + credits;
-				sendDataToExtension(credits);
-			}
-			close();
-		}, 1000);
-	}, 1000);
-	
-} else{
-	// Do nothing
-}
-
-
-/*
-Functions referenced on page load
-*/
-
+// Gets the targetId from the extension
 function getIdFromExtension(request){
-	chrome.runtime.sendMessage({greeting: request}, function(response) { // Gets the data from the extension
+	chrome.runtime.sendMessage({greeting: request}, function(response){ 
 		targetId = (response.farewell);
+		return targetId;
 	});
 }
-
+// Gets the key from the extension
 function getCreditsFromExtension(request){
-	chrome.runtime.sendMessage({greeting: request}, function(response) { // Gets the data from the extension
-		credits = (response.farewell);
+	chrome.runtime.sendMessage({greeting: request}, function(response){
+		key = (response.farewell);
 	});
 }
-
+// Sends the data to the extension
 function sendDataToExtension(request){
-	chrome.runtime.sendMessage({greeting: request}, function(response) { // Sends the data to the extension
-		console.log(response.farewell);
+	chrome.runtime.sendMessage({greeting: request}, function(response){
 	});
 }
-
+// Key registered as credits
 function obtainCredits(targetId){
-	// Extension has completed loading the imdb page
-	console.log(targetId);
+	// Obtain the node containing the target value
 	node = document.getElementById(targetId);
-	
 	// Convert the node to a string
 	var tmpNode = document.createElement( "div" );
 	tmpNode.appendChild( node.cloneNode( true ) );
-	var str = tmpNode.innerHTML;
-	//tmpNode = node = null; // prevent memory leaks in IE
-	
+	var str = tmpNode.innerHTML;	
 	// Obtain the credits from the string
 	var innerHTML = str.substr(0, str.indexOf('credit'));
 	var last5 = innerHTML.slice(-5);
 	var tmpArr = last5.match(/\d+/g).map(Number);
-	credits = tmpArr[0];
+	// Sets the key to the appropriate number of credits
+	key = tmpArr[0];
+	return key;
 }
-
+// Key registered as running time
 function obtainRunningTime(){
-	// Extension has completed loading the imdb page
+	// Obtain the node containing the target value
 	node = document.querySelector("time");
-	if (node === null) { // The page does not have a time setActive
-		runningTime = "0";
+	if (node === null) {
+		// The page does not have a time setActive, default to zero
+		key = "0";
 	} else {
 		// Convert the node to a string
 		var tmpNode = document.createElement( "div" );
 		tmpNode.appendChild( node.cloneNode( true ) );
 		var str = tmpNode.innerHTML;
-		//tmpNode = node = null; // prevent memory leaks in IE
-		
 		// Obtain the running time from the string
 		var innerHTML = str.substr(0, str.indexOf('min'));
 		var last5 = innerHTML.slice(-5);
@@ -142,117 +65,190 @@ function obtainRunningTime(){
 		var timeArray = time.split(" ");
 		var minutes
 		if (timeArray[0] === 0) { // Time is less than one hour
-			minutes = timeArray[3];
+			key = timeArray[3];
 		} else {
-			minutes = parseInt(timeArray[0])*60+parseInt(timeArray[1]);
+			key = parseInt(timeArray[0])*60+parseInt(timeArray[1]);
 		}
-		// would be 90
-		runningTime = minutes;
 	}
 }
-
-function getTargetId(){
-	// Get the target
-	targetId = "";
-	var target = document.querySelectorAll("strong")[1].innerHTML.replace(/['"]+/g, '');
-	
-	if (target === "Tips:") { // We caught a running length listing
-		console.log("We need running time");
-		targetId = ("Tips:");
-		return targetId;
-	}	
-	
-	/*
-	Here we have recieved the target ie actor, actress, producer
-	What we need to do is correspond it with the appropriate ending
-	*/
-	if (target == "Actor"){
-		target = "actor";
-	} else if (target == "Actress"){
-		target = "actress";
-	} else if (target == "Writer"){
-		target = "writer";
-	} else if (target == "Producer"){
-		target = "producer";
-	} else if (target == "Miscellaneous Crew"){
-		target = "miscellaneous";
-	} else if (target == "Music Department"){
-		target = "music_department";
-	} else if (target == "Thanks"){
-		target = "thanks";
-	} else if (target == "Self"){
-		target = "self";
-	} else if (target == "Stunts"){
-		target = "stunts";
-	} else if (target == "Art Department"){
-		target = "art_department";
-	} else if (target == "Location Management"){
-		target = "location_management";
-	} else if (target == "Second Unit Director or Assistant Director"){
-		target = "assistant_director";
-	} else if (target == "Director"){
-		target = "director";
-	} else if (target == "Archive Footage"){
-		target = "archive_footage";
-	} else if (target == "Cinematographer"){
-		target = "cinematographer";
-	} else if (target == "Sound Department"){
-		target = "sound_department";
-	} else if (target == "Visual Effects"){
-		target = "visual_effects";
-	} else if (target == "Camera And Electrical Department"){
-		target = "camera_department";
-	} else if (target == "Special Effects"){
-		target = "special_effects";
-	} else if (target == "Production Designer"){
-		target = "production_designer";
-	} else if (target == "Production Manager"){
-		target = "production_manager";
-	} else {
-		target = "You need to add more types";
-	}
-	// Attach the two
-	var targetIdPrefix = "filmo-head-";
-	targetId = targetIdPrefix.concat(target);
-	return targetId;
-}
-
+// Gets the next available IMDb listing
 function getListing(){
-	// Get the first IMDB listing ID
+	// Search for an IMDb tag
 	var listingId = document.querySelector(".imdb-yellow-font").parentElement.parentElement.getAttribute("data-listing-id");
-	// Obtain the listing URL
-	var listingPrefix = "http://www.karmalicity.com/get-points/listing/?id=";
-	// Combine the two
-	var listingURL = listingPrefix.concat(listingId);
+	// Listing URL prefix is known
+	var listingURL = "http://www.karmalicity.com/get-points/listing/?id=".concat(listingId);
 	// Return the listingURL
 	return listingURL;
 }
-
-// this function can fire onclick handler for any DOM-Element
-function fireClickEvent(element) {
-    var evt = new window.MouseEvent('click', {
+// Gets the targetId
+function getTargetId(){
+	// Clear the target for fresh grab
+	targetId = "";
+	// Get the target
+	var target = document.querySelectorAll("strong")[1].innerHTML.replace(/['"]+/g, '');
+	// We caught a running length listing
+	if(target === "Tips:"){
+		targetId = ("Tips:");
+	}else if(target == "Actor"){
+		targetId = "filmo-head-actor";
+	}else if(target == "Actress"){
+		targetId = "filmo-head-actress";
+	}else if(target == "Writer"){
+		targetId = "filmo-head-writer";
+	}else if(target == "Producer"){
+		targetId = "filmo-head-producer";
+	}else if(target == "Miscellaneous Crew"){
+		targetId = "filmo-head-miscellaneous";
+	}else if(target == "Music Department"){
+		targetId = "filmo-head-music_department";
+	}else if(target == "Thanks"){
+		targetId = "filmo-head-thanks";
+	}else if(target == "Self"){
+		targetId = "filmo-head-self";
+	}else if(target == "Stunts"){
+		targetId = "filmo-head-stunts";
+	}else if(target == "Art Department"){
+		targetId = "filmo-head-art_department";
+	}else if(target == "Location Management"){
+		targetId = "filmo-head-location_management";
+	}else if(target == "Second Unit Director Or Assistant Director"){
+		targetId = "filmo-head-assistant_director";
+	}else if(target == "Director"){
+		targetId = "filmo-head-director";
+	}else if(target == "Archive Footage"){
+		targetId = "filmo-head-archive_footage";
+	}else if(target == "Cinematographer"){
+		targetId = "filmo-head-cinematographer";
+	}else if(target == "Sound Department"){
+		targetId = "filmo-head-sound_department";
+	}else if(target == "Visual Effects"){
+		targetId = "filmo-head-visual_effects";
+	}else if(target == "Camera And Electrical Department"){
+		targetId = "filmo-head-camera_department";
+	}else if(target == "Special Effects"){
+		targetId = "filmo-head-special_effects";
+	}else if(target == "Production Designer"){
+		targetId = "filmo-head-production_designer";
+	}else if(target == "Production Manager"){
+		targetId = "filmo-head-production_manager";
+	}else if(target == "Casting Director"){
+		targetId = "filmo-head-casting_director";
+	}else if(target == "Costume And Wardrobe Department"){
+		targetId = "filmo-head-costume_department";
+	}else if(target == "Casting Director"){
+		targetId = "filmo-head-casting_director";
+	}else if(target == "Casting Department"){
+		targetId = "filmo-head-casting_department";
+	}else{
+		targetId = "The developer needs to add more types";
+		alert(targetId);
+		console.log(targetId);
+	}
+	return targetId;
+}
+// This function can fire onclick handler for any DOM-Element
+function fireClickEvent(element){
+    var evt = new window.MouseEvent('click',{
         view: window,
         bubbles: true,
         cancelable: true
     });
-
     element.dispatchEvent(evt);
 }
-
-// this function will setup a virtual anchor element
-// and fire click handler to open new URL in the same room
-// it works better than location.href=something or location.reload()
-function openNewURLInTheSameWindow(targetURL) {
+// Opens URL in the same window
+function openNewURLInTheSameWindow(targetURL){
     var a = document.createElement('a');
     a.href = targetURL;
     fireClickEvent(a);
 }
-
-function openNewTabOrNewWindow(targetURL) {
+// Opens URL in new tab
+function openNewTabOrNewWindow(targetURL){
     var a = document.createElement('a');
     a.href = targetURL;
-
-    a.target = '_blank'; // now it will open new tab/window and bypass any popup blocker!
-
+    a.target = '_blank';
     fireClickEvent(a);
+}
+// Checks if the daily limit has been reached
+function checkDailyLimit(){
+	var activityCol = document.getElementById("rightColumn");
+	var tmpNode = document.createElement( "div" );
+	tmpNode.appendChild( activityCol.cloneNode( true ));
+	var str = tmpNode.innerHTML;
+	if (str.includes("You've completed 60 out of 60 possible actions so far today.")) { // completed daily activity
+		// Daily limit reached
+		dLR = true;
+	}else{
+		// Daily limit not yet reached
+		dLR - false;
+	}
+}
+
+/*
+Page Validator
+Checks which page we are currently loaded on.
+*/
+if(title == "http://www.karmalicity.com/get-points/"){
+	// Gets the url of the next IMDb listing
+	var urlListing = getListing(); 
+	// Opens the listing on the current page
+	if(urlListing.includes("null")){
+		console.log(urlListing);
+		// There currently is no IMDb listing
+		// Implement a waiting period.
+	}
+	openNewURLInTheSameWindow(urlListing);
+}else if(title.includes("listing")){
+	// Check if the daily limit has been reached
+	checkDailyLimit();
+	if(dLR){
+		// Disable the extension
+		// Implement a method of turning off or disabling hte extension
+		alert("You have reached your limit for Daily Actions!");
+	}else{
+		// Gets the target id
+		targetId = getTargetId();
+		// Sends the targetId to the extension
+		sendDataToExtension(targetId);
+		// Opens up the IMDb page
+		document.querySelector(".btn-karma").click();
+		console.log(targetId);
+		
+		/*
+		Extension should now be on IMDb page
+		*/
+		
+		// Allow the extension to gather data from IMDb page
+		setTimeout(function(){
+			getCreditsFromExtension("credits");
+			setTimeout(function(){
+				console.log(key);
+				key = key.replace('key_','');
+				document.getElementById("imdb-view-answer").setAttribute('value',key); // Fills credits
+				document.querySelector(".btn-success").click(); // Click the button
+			}, 1000);
+		}, 4000);
+	}
+	
+}else if(title.includes("imdb")){
+	// Get the targetId from the extension
+	setTimeout(function(){
+		targetId = getIdFromExtension("target");
+		setTimeout(function(){
+			console.log(targetId);
+			if (targetId == "Tips:" || targetId == undefined){
+				// Running time listing
+				obtainRunningTime();
+				key = "key_" + key;
+				sendDataToExtension(key);
+			} else{
+				// Credit listing
+				obtainCredits(targetId);
+				key = "key_" + key;
+				sendDataToExtension(key);
+			}
+			close();
+		}, 1000);
+	}, 1000);
+}else{
+	// We are on some other page, do nothing
 }
