6cfe0bd35a9d0043be485fe374e11d7fdc9ba2f8
Aaron Maynard <34851708+aaronmaynard@users.noreply.github.com>
2018-08-07 04:49:51 -0500

    Cleaned and removed redundant code

--- assistant/background.js
+++ assistant/background.js
@@ -1,83 +1,63 @@
 /*
-Global variables
+Global Variables
+These variables are used to carry data between the background scripts.
 */
-
-var title; // Gets the url of the webpage
-var credits; // Setting the default value to zero
-var targetId; // The target used to collect the credits on IMDb
+var credits; // The number of credits obtained from IMDb
+var targetId; // The string used to find the corresponding DIV on IMDb
 
 /*
-Main stuff
+Message Passing
+Send a one-time JSON-serializable message from a content script to extension , or vice versa, respectively.
 */
+function messagePassing(){
+	chrome.runtime.onMessage.addListener(
+		function(request, sender, sendResponse){
+			// Content script [Karmalicity] is passing the variable targetId
+			if(request.greeting.includes("filmo-head-")){
+				// Set the incoming request to the targetId
+				targetId = request.greeting;
+			}
+			// Extension sends the targetId to the content script [IMDb]
+			if(request.greeting.includes("target")){
+				// Sends the targetId
+				sendResponse({farewell: (targetId)});
+				// Reset the targetId
+				targetId = "";
+			}
+			// Content script [IMDb] is passing the credits to the extension
+			if(request.greeting.includes("key_")){
+				// Set the incoming request to the variable credits
+				credits = request.greeting;
+			}
+			// Extension sends the credits to the content script [Karmalicity]
+			if(request.greeting.includes("credits")){
+				// Set the incoming request to the variable credits
+				sendResponse({farewell: (credits)});
+			}
+		}
+	);
+}
 
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    console.log(sender.tab ?
-                "from a content script:" + sender.tab.url :
-                "from the extension");
-	if (request.greeting.includes("filmo-head-")){
-		targetId = request.greeting;
-		sendResponse({farewell: ("received targetId: " + targetId)});
-	}
-	if (request.greeting.includes("target")){
-		sendResponse({farewell: (targetId)});
-		targetId = "";
-	} 
-	if (request.greeting.includes("key_")){
-		credits = request.greeting;
-		sendResponse({farewell: (credits)});
-	} 
-	if (request.greeting.includes("credits")){
-		sendResponse({farewell: (credits)});
-	} 
-});
-  
-  
+/*
+Extension Runner
+Starts the extension when the icon is clicked.  Navigates to Karmalicity and runs the Message Passing sequence.
+*/
 chrome.browserAction.onClicked.addListener(function(activeTab){
-    // Attempt to open Karmalicity get points page
-    var getPoints = "http://www.karmalicity.com/get-points/";
-    chrome.tabs.create({ url: getPoints });
-
-	// Allow the browser to redirect if the user is not logged in
-    setTimeout(function(){
-		var check;
-		// Check if the user is logged in
-		chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-			// Boolean returns true if the url contains the string 'login'
-			check = (tabs[0].url).includes('login');
-
-			if (check == true){
-				// If returns true, user is not logged in
+	// Opens the "Get Points" page on Karmalicity
+	chrome.tabs.create({ url: "http://www.karmalicity.com/get-points/" });
+	// One second timeout to allow the page to redirect if the user is not logged in
+	setTimeout(function(){		
+		// Runs a query to obtain tab information
+		chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+			// Checks if the user is logged in
+			var check = (tabs[0].url).includes('login');
+			if(check){
+				// The user is not logged in
 				alert("You need to be logged in to utilize this application!\nPlease log in and try again.");
-			}else { 
-				// User is logged in
-				chrome.runtime.onMessage.addListener(
-					function(request, sender, sendResponse) {
-						console.log(sender.tab ?
-									"from a content script:" + sender.tab.url :
-									"from the extension");
-						if (request.greeting.includes("filmo-head-")){
-							targetId = request.greeting;
-							sendResponse({farewell: ("received targetId: " + targetId)});
-						}
-						if (request.greeting.includes("Tips")){
-							targetId = request.greeting;
-							sendResponse({farewell: ("received targetId: " + targetId)});
-						}
-						if (request.greeting.includes("target")){
-							sendResponse({farewell: (targetId)});
-							targetId = "";
-						} 
-						if (request.greeting.includes("key_")){
-							credits = request.greeting;
-							sendResponse({farewell: (credits)});
-						} 
-						if (request.greeting.includes("credits")){
-							sendResponse({farewell: (credits)});
-						} 
-					}
-				);
+			}else{
+				// The user is logged in
+				messagePassing();
 			}
 		});
-    }, 1000);
+	}, 1000);
 });
